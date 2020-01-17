4ea1d9fe6f86f1beac202f93963abdb808dde985
weatbix-kid <weatbixz@gmail.com>
2020-01-17 15:01:24 +1300

    Clean up

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import axios from 'axios'
-
 Vue.config.productionTip = false
 
 new Vue({
-    data() {
-      return {
-        testRootData: 'Hello world',
-      }
-    },
-    methods: {
-      printTestData(){
-        console.log(this.testRootData)
-      }
-    },
-  // axios,
+  data() {
+    return {
+      testRootData: 'Hello world',
+    }
+  },
+  methods: {
+    printTestData(){
+      console.log(this.testRootData)
+    }
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
